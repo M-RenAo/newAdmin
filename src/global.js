474c9c614908724ad8e8dbf416de9c9caf64dc74
M-RENAO <1156845285@qq.com>
2018-11-27 11:08:26 +0800

    ceshi

--- src/global.js
+++ src/global.js
@@ -2,7 +2,7 @@
 // window.BaseUrlApp='https://imapp.io/app/'
 //
 window.BaseUrl='http://52.230.122.251:36788/admin/'
-// window.BaseUrlApp='http://52.230.122.251:36786/app/'
+window.BaseUrlApp='http://52.230.122.251:36786/app/'
 // window.BaseUrl='http://192.168.50.5:36788/admin/'
 // window.BaseUrlApp='http://192.168.50.5:36786/app/'
 
